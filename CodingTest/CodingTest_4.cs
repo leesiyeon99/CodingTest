411f6866a64a25ceeaf90fee0e469e9aba7f2d25
leesiyeon99 <qsqs9090@naver.com>
2024-07-19 14:12:22 +0900

    [Edit] CodingTest_4

--- CodingTest/CodingTest_4.cs
+++ CodingTest/CodingTest_4.cs
@@ -8,7 +8,8 @@ namespace CodingTest
 {
     internal class CodingTest_4
     {
-        //정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+        //정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에
+        //1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
         public class Solution
         {
             public int solution(int num1, int num2)
