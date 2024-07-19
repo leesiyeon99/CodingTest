29e8bbc208a54ae6d3e6c2c6cf8fcf0fe114ed4a
leesiyeon99 <qsqs9090@naver.com>
2024-07-19 12:19:40 +0900

    [Add] CodingTest_4

---
+++ CodingTest/CodingTest_4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class CodingTest_4
+    {
+        //정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+        public class Solution
+        {
+            public int solution(int num1, int num2)
+            {
+                int answer = -1;
+                return answer = num1 % num2;
+            }
+        }
+    }
+}
